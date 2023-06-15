a521b89857ab9a440ead2069efaae1123e8e9cb5
Peter Adeyemo <adeyemo@tm30.net>
2023-06-15 11:41:13 +0100

    Add sql alchemy config and jwt secret

--- Backend/api/v1/__init__.py
+++ Backend/api/v1/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 import os
 from api.v1.routes.auth import auth
 from api.v1.routes.review import review
+from api.v1.routes.places import places
 from api.v1.models import db
+from flask_jwt_extended import JWTManager
 
 
 def create_app(test_config=None):
@@ -13,6 +15,8 @@ def create_app(test_config=None):
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
         )
 
     else:
@@ -21,7 +25,10 @@ def create_app(test_config=None):
     db.app = app
     db.init_app(app)
 
+    JWTManager(app)
+
     app.register_blueprint(auth)
     app.register_blueprint(review)
+    app.register_blueprint(places)
 
     return app
